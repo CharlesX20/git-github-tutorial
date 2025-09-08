61010563ecf38ad83866dfc99f395b64242e9ba6
CharlesGPT <chimecharles23@gmail.com>
2025-09-08 09:54:52 -0400

    New code...

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-// let stringCharacter = 'Asap Frontend';
 
-// for (i = 0; i < stringCharacter.length; i++ ){
-//     console.log(stringCharacter[i])
-// }
 
-// function converter(naira){
-//     let dollar = naira / 1100
-//     return `Your money in dollars is $${dollar}`
-// };
+document.getElementById('myButton').onclick = function(){
+    let myUserName = document.getElementById('name').value;
+    document.getElementById('myName').textContent = `Hello ${myUserName}`;
 
-// const nairaConverter = (naira) => {
-//     let dollar = naira/1100;
-//     return `#${naira} = $${dollar}`;
-// };
 
-// console.log(nairaConverter(22000));
-
-// let arrayItems = ["Apples", "Banana", "Oranges", "Cake"];
-// console.log(arrayItems)
-// arrayItems.push("Bread");
-// console.log(arrayItems)
-// arrayItems.filter((items) => {
-//     console.log(items)
-// })
-
-// function openDiv(){
-//     document.querySelector('body').classList.toggle('body-class')
-// }
+}
