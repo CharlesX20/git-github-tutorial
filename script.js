7f852e514541a70bbfa582c4354f79f28bc0cad8
CharlesGPT <chimecharles23@gmail.com>
2025-09-08 12:38:48 -0400

    New Edit

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+let counter = 0;
+const counterNumber = document.getElementById('counterResult');
+const increaseBtn = document.getElementById('increase-btn');
+const resetBtn = document.getElementById('reset-btn');
+const decreaseBtn = document.getElementById('decrease-btn');
 
+increaseBtn.onclick = function(){
+    counter = counter + 1;
+    counterNumber.textContent = counter;
+}
+resetBtn.onclick = function(){
+    counterNumber.textContent = counter = 0;
+}
+decreaseBtn.onclick = function(){
+    counter = counter - 1;
+    counterNumber.textContent = counter;
+}
+//dice program
+const rollButton = document.getElementById('diceButton');
+const dice1 = document.getElementById('diceNumber1');
+const dice2 = document.getElementById('diceNumber2');
+let randomNumber1;
+let randomNumber2;
 
-document.getElementById('myButton').onclick = function(){
-    let myUserName = document.getElementById('name').value;
-    document.getElementById('myName').textContent = `Hello ${myUserName}`;
+rollButton.onclick = function(){
+  randomNumber1 = Math.floor(Math.random()*6) + 1;
+  randomNumber2 = Math.floor(Math.random()*6) + 1;
 
-
+  dice1.textContent = '🎲'.repeat(randomNumber1);
+  dice2.textContent = '🎲'.repeat(randomNumber2);
 }
