042f6b6731d5631906532b787e155c63108e8e1c
CharlesGPT <chimecharles23@gmail.com>
2025-09-15 11:01:17 -0400

    My recent commit

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-const userNumber = document.querySelector("#userNumber");
+const currentUnit = document.querySelector("#currentUnit");
+const kgConverter = document.querySelector("#kgConverter");
+const gConverter = document.querySelector("#gConverter");
 const submitButton = document.querySelector(".submitButton");
-const userNumberOutput = document.querySelector(".userNumberOutput");
-const userNumberOutputResult = document.querySelector(".userNumberOutputResult");
 const endResult = document.querySelector(".endResult");
+let finalResult;
 
-let minNumber = 1;
-let maxNumber = 100;
-let attempts = 0;
-let randomNumber = Math.floor(Math.random() * maxNumber) + minNumber;
-
-submitButton.onclick = function() {
-  attempts++;
-  userNumberValue = Number(userNumber.value);
-
-  if (!isNaN(userNumberValue)) {
-    userNumberOutput.textContent = `You entered ${userNumberValue}`;
-
-    if (userNumberValue === randomNumber) {
-    userNumberOutputResult.style.backgroundColor = "green";
-      userNumberOutputResult.textContent = "Correct!";
-      endResult.textContent = `The correct answer was ${randomNumber} and it took you ${attempts} attempts`;
-      // You can add code here to reset the game or disable the button
-    } else if (userNumberValue < randomNumber) {
-        userNumberOutputResult.style.backgroundColor = "red";
-      userNumberOutputResult.textContent = "Incorrect!";
-      endResult.textContent = `${userNumberValue} is lower, Please try again!`;
-    } else {
-        userNumberOutputResult.style.backgroundColor = "red";
-      userNumberOutputResult.textContent = "Incorrect!";
-      endResult.textContent = `${userNumberValue} is higher, Please try again!`;
+function convert(){
+    if(kgConverter.checked){
+        finalResult = Number(currentUnit.value)
+        finalResult = finalResult * 1000
+        endResult.textContent = `The answer is ${finalResult}g`
     }
-  } else {
-    userNumberOutput.textContent = "Please enter a number!";
-  }
-
-  userNumber.value = "";
+    else if(gConverter.checked){
+        finalResult = Number(currentUnit.value)
+        finalResult = finalResult/1000
+        endResult.textContent = `The answer is ${finalResult}kg`
+    }
+    else{
+        endResult.textContent = `No conversion was checked!`
+        
+    }
 }
+
+function sumNumbers(...allNumbers){
+    let result = 0;
+    for (let number of allNumbers){
+        result += number;
+    }
+    console.log(result)
+}
+sumNumbers(1,6)
